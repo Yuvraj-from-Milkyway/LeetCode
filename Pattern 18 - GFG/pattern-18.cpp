933a0007786e97d847e9f4015691e8642d9db4eb
Yuvraj-from-Milkyway <80397970+Yuvraj-from-Milkyway@users.noreply.github.com>
2023-03-11 07:42:21 +0530

    Added solution - LeetHub

---
+++ Pattern 18 - GFG/pattern-18.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        int i;
+        char j;
+        for(i = 1; i <= n; i++){
+            for(j = ('A'+n-1); j >= ('A'+n-i); j--){
+              cout << j << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
